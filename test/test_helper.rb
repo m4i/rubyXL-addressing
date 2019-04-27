bfc32d36e92dd8e1f391ed200711f72b972d36fa
Masaki Takeuchi <m.ishihara@gmail.com>
2019-04-28 00:15:15 +0900

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/test/'
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'minitest/autorun'
